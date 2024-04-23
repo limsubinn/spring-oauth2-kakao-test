1212c40aea7d9bbfa5efd951a74fadac9e487c07
limsubinn <mymyx02@naver.com>
2024-04-23 16:37:52 +0900

    Fix: OAuth2UserInfo - 불필요한 정보 삭제

--- src/main/java/com/example/kakaologin/oauth2/model/KakaoOAuth2UserInfo.java
+++ src/main/java/com/example/kakaologin/oauth2/model/KakaoOAuth2UserInfo.java
@@ -7,18 +7,14 @@ import java.util.Map;
 public class KakaoOAuth2UserInfo implements OAuth2UserInfo {
 
     private OAuth2Provider provider;
-    private String id;
     private String email;
     private String profileImgUrl;
-    private String accessToken;
 
     @Builder
     private KakaoOAuth2UserInfo(OAuth2Provider provider, String id, String email, String profileImgUrl, String accessToken) {
         this.provider = provider;
-        this.id = id;
         this.email = email;
         this.profileImgUrl = profileImgUrl;
-        this.accessToken = accessToken;
     }
 
     public static KakaoOAuth2UserInfo of(Map<String, Object> attributes, String accessToken) {
@@ -27,7 +23,6 @@ public class KakaoOAuth2UserInfo implements OAuth2UserInfo {
 
         return builder()
                 .provider(OAuth2Provider.KAKAO)
-                .id(((Long) attributes.get("id")).toString())
                 .email((String) kakaoAccount.get("email"))
                 .profileImgUrl((String) kakaoProfile.get("profile_image_url"))
                 .accessToken(accessToken)
@@ -37,16 +32,6 @@ public class KakaoOAuth2UserInfo implements OAuth2UserInfo {
     @Override
     public OAuth2Provider getProvider() {
         return provider;
-    }
-
-    @Override
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    @Override
-    public String getId() {
-        return id;
     }
 
     @Override
--- src/main/java/com/example/kakaologin/oauth2/model/OAuth2UserInfo.java
+++ src/main/java/com/example/kakaologin/oauth2/model/OAuth2UserInfo.java
@@ -3,9 +3,7 @@ package com.example.kakaologin.oauth2.model;
 public interface OAuth2UserInfo {
 
     OAuth2Provider getProvider();
-    String getId();
     String getEmail();
-    String getAccessToken();
     String profileImgUrl();
 
 }
