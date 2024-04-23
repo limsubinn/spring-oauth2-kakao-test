e7f21adf924ce4de79c3b4d61e7d24e20a440fc3
limsubinn <mymyx02@naver.com>
2024-04-23 16:53:45 +0900

    Refactor: OAuth2AuthorizationRepository, CookieUtils 분리

--- src/main/java/com/example/kakaologin/oauth2/repository/OAuth2AuthorizationRepository.java
+++ src/main/java/com/example/kakaologin/oauth2/repository/OAuth2AuthorizationRepository.java
@@ -1,17 +1,13 @@
 package com.example.kakaologin.oauth2.repository;
 
-import jakarta.servlet.http.Cookie;
+import com.example.kakaologin.oauth2.utils.CookieUtils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.util.SerializationUtils;
 import org.springframework.util.StringUtils;
-
-import java.util.Base64;
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -22,11 +18,13 @@ public class OAuth2AuthorizationRepository implements AuthorizationRequestReposi
     public static final String MODE_PARAM_COOKIE_NAME = "mode";
     public static final int COOKIE_EXPIRE_SECONDS = 100;
 
+    private final CookieUtils cookieUtils;
+
     @Override
     public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
         // 쿠키 정보 꺼내기
-        return getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
-                .map(cookie -> deserialize(cookie, OAuth2AuthorizationRequest.class))
+        return cookieUtils.getCookie(request, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME)
+                .map(cookie -> cookieUtils.deserialize(cookie, OAuth2AuthorizationRequest.class))
                 .orElse(null);
     }
 
@@ -37,15 +35,15 @@ public class OAuth2AuthorizationRepository implements AuthorizationRequestReposi
         }
 
         // 쿠키 저장
-        addCookie(response,
+        cookieUtils.addCookie(response,
                 OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME,
-                serialize(authorizationRequest),
+                cookieUtils.serialize(authorizationRequest),
                 COOKIE_EXPIRE_SECONDS);
 
         // redirect uri
         String redirectUri = request.getParameter(REDIRECT_URI_PARAM_COOKIE_NAME);
         if (StringUtils.hasText(redirectUri)) {
-            addCookie(response,
+            cookieUtils.addCookie(response,
                     REDIRECT_URI_PARAM_COOKIE_NAME,
                     redirectUri,
                     COOKIE_EXPIRE_SECONDS);
@@ -54,7 +52,7 @@ public class OAuth2AuthorizationRepository implements AuthorizationRequestReposi
         // mode (login, logout, unlink)
         String mode = request.getParameter(MODE_PARAM_COOKIE_NAME);
         if (StringUtils.hasText(mode)) {
-            addCookie(response,
+            cookieUtils.addCookie(response,
                     MODE_PARAM_COOKIE_NAME,
                     mode,
                     COOKIE_EXPIRE_SECONDS);
@@ -67,55 +65,9 @@ public class OAuth2AuthorizationRepository implements AuthorizationRequestReposi
     }
 
     public void removeAuthorizationRequestCookies(HttpServletRequest request, HttpServletResponse response) {
-        deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
-        deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
-        deleteCookie(request, response, MODE_PARAM_COOKIE_NAME);
-    }
-
-    private Optional<Cookie> getCookie(HttpServletRequest request, String name) {
-
-        Cookie[] cookies = request.getCookies();
-
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if(cookie.getName().equals(name))
-                    return Optional.of(cookie);
-            }
-        }
-
-        return Optional.empty();
-    }
-
-    private void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
-        Cookie cookie = new Cookie(name, value);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-        cookie.setMaxAge(maxAge);
-        response.addCookie(cookie);
-    }
-
-    private void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if(cookie.getName().equals(name)) {
-                    cookie.setValue("");
-                    cookie.setPath("/");
-                    cookie.setMaxAge(0);
-                    response.addCookie(cookie);
-                }
-            }
-        }
-    }
-
-    private String serialize(Object object) {
-        return Base64.getUrlEncoder()
-                .encodeToString(SerializationUtils.serialize(object));
-    }
-
-    private <T> T deserialize(Cookie cookie, Class<T> cls) {
-        return cls.cast(SerializationUtils.deserialize(
-                Base64.getUrlDecoder().decode(cookie.getValue())));
+        cookieUtils.deleteCookie(request, response, OAUTH2_AUTHORIZATION_REQUEST_COOKIE_NAME);
+        cookieUtils.deleteCookie(request, response, REDIRECT_URI_PARAM_COOKIE_NAME);
+        cookieUtils.deleteCookie(request, response, MODE_PARAM_COOKIE_NAME);
     }
 
 }
