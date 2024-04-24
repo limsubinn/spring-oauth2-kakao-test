fa27f7ca1fcc232a65a7f4a70289a1d9adfcf46a
limsubinn <mymyx02@naver.com>
2024-04-25 02:24:29 +0900

    Fix: 로그인 실패 처리
    - 쿠키에서 redirect_uri 추출 후 실패 정보 리다이렉트

--- src/main/java/com/example/kakaologin/oauth2/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/example/kakaologin/oauth2/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,23 +1,50 @@
 package com.example.kakaologin.oauth2.handler;
 
-import com.example.kakaologin._common.exception.BusinessException;
+import com.example.kakaologin._common.utils.CookieUtils;
+import com.example.kakaologin.oauth2.repository.OAuth2AuthorizationRepository;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import static com.example.kakaologin._common.response.status.ErrorType.OAUTH_AUTHENTICATION_FAIL;
+import java.io.IOException;
 
-@Component
+import static com.example.kakaologin.oauth2.repository.OAuth2AuthorizationRepository.REDIRECT_URI_PARAM_COOKIE_NAME;
+
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    private final OAuth2AuthorizationRepository oAuth2AuthorizationRepository;
+    private final CookieUtils cookieUtils;
+
     @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
-        // 인증 실패
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException {
         log.error("oauth2 인증 실패 : {}", exception.getMessage());
-        throw new BusinessException(OAUTH_AUTHENTICATION_FAIL);
+
+        // redirect uri
+        String targetUrl = UriComponentsBuilder.fromUriString(getRedirectUriFromRequest(request))
+                .queryParam("error", exception.getLocalizedMessage())
+                .build().toUriString();
+
+        // 쿠키 삭제
+        oAuth2AuthorizationRepository.removeAuthorizationRequestCookies(request, response);
+
+        // 리다이렉트
+        response.sendRedirect(targetUrl);
+    }
+
+    private String getRedirectUriFromRequest(HttpServletRequest request) {
+        return cookieUtils.getCookie(request, REDIRECT_URI_PARAM_COOKIE_NAME)
+                .map(Cookie::getValue)
+                .orElse("http://localhost:5173/login-callback");
     }
 
 }
