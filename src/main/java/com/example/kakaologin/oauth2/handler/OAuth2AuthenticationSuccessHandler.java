c9f5d82665f20b8b0b7524d99b38fa5e5c663609
limsubinn <mymyx02@naver.com>
2024-04-25 00:52:27 +0900

    Feat: 로그인 및 회원가입 처리 (리프레쉬 토큰 저장 필요함)

--- src/main/java/com/example/kakaologin/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/example/kakaologin/oauth2/handler/OAuth2AuthenticationSuccessHandler.java
@@ -5,6 +5,7 @@ import com.example.kakaologin._common.utils.JwtUtils;
 import com.example.kakaologin.member.entity.Member;
 import com.example.kakaologin.member.service.MemberService;
 import com.example.kakaologin.oauth2.model.OAuth2UserPrincipal;
+import com.example.kakaologin.oauth2.repository.OAuth2AuthorizationRepository;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,6 +26,7 @@ import static com.example.kakaologin.oauth2.repository.OAuth2AuthorizationReposi
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final OAuth2AuthorizationRepository oAuth2AuthorizationRepository;
     private final CookieUtils cookieUtils;
     private final JwtUtils jwtUtils;
     private final MemberService memberService;
@@ -32,20 +34,22 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
+        // 이미 http 응답이 클라이언트로 보내진 경우
+        if (response.isCommitted()) {
+            return;
+        }
 
-
+        // 쿠키 삭제
+        clearAuthenticationAttributes(request, response);
+        getRedirectStrategy().sendRedirect(request, response, getTargetUrl(request, authentication));
     }
 
-    private String getTargetUrl(HttpServletRequest request, HttpServletResponse response,
-                                  Authentication authentication) {
+    private String getTargetUrl(HttpServletRequest request, Authentication authentication) {
         // redirect uri
         String redirectUri = getRedirectUriFromRequest(request);
 
-        // mode
-        String mode = getModeFromRequest(request); // login, logout, unlink
-        if (mode == null) {
-            return getFailUrl(redirectUri);
-        }
+        // login, logout, unlink
+        String mode = getModeFromRequest(request);
 
         // 유저 인증 정보
         OAuth2UserPrincipal principal = getOAuth2UserPrincipal(authentication);
@@ -60,29 +64,17 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                     principal.getUserInfo().getProvider(),
                     principal.getUserInfo().getEmail());
 
-            Long memberId;
-            String nextPage = "";
-
+            // 로그인
             if (member != null) {
-                // 로그인
-                memberId = member.getId();
-                nextPage = "login";
-            }
-            else {
-                // 회원가입
-                memberId = memberService.save(principal);
-                nextPage = "sign-up";
+                return getLoginUrl(authentication, redirectUri, member.getId(), "login");
             }
 
-            // 회원가입 or 로그인 페이지로 리다이렉트
-            return UriComponentsBuilder.fromUriString(redirectUri)
-                    .queryParam("access-token", jwtUtils.generateAccessToken(authentication, memberId))
-                    .queryParam("refresh-token", jwtUtils.generateRefreshToken(authentication, memberId))
-                    .queryParam("next-page", nextPage)
-                    .build().toUriString();
+            // 회원가입
+            getLoginUrl(authentication, redirectUri, memberService.save(principal), "sign-up");
         }
 
         // TODO: 회원탈퇴, 로그아웃 로직 구현
+        return getFailUrl(redirectUri);
     }
 
     private String getRedirectUriFromRequest(HttpServletRequest request) {
@@ -111,6 +103,19 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         return UriComponentsBuilder.fromUriString(redirectUri)
                 .queryParam("error", "login-fail")
                 .build().toUriString();
+    }
+
+    private String getLoginUrl(Authentication authentication, String redirectUri, Long memberId, String nextPage) {
+        return UriComponentsBuilder.fromUriString(redirectUri)
+                .queryParam("access-token", jwtUtils.generateAccessToken(authentication, memberId))
+                .queryParam("refresh-token", jwtUtils.generateRefreshToken(authentication, memberId))
+                .queryParam("next-page", nextPage)
+                .build().toUriString();
+    }
+
+    private void clearAuthenticationAttributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        oAuth2AuthorizationRepository.removeAuthorizationRequestCookies(request, response);
     }
 
 }
